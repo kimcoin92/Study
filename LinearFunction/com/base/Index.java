ae59feed0cbb12f37ddc099d185ffc9527bab7df
Kimcoin <kimcoin92@gmail.com>
2022-05-31 11:56:49 +0900

    STYLE 패키지 분리, 캡슐화

--- LinearFunction/com/base/Index.java
+++ LinearFunction/com/base/Index.java
@@ -1,18 +1,18 @@
+// 일차함수 계산 로직
 // 2. 메인 클래스
+
 // 마지막 출력을 위한 본 클래스
+package com.base;
 
-package main;
+import com.system.*;
 
-public class Main {
+public class Index {
 
 	public static void main(String[] args)
 	{
-		// 의문점 : 각각의 값에 new가 적힌 이유
-		// 없으면 타입이 없다는 오류 발생
+		// 의문점 : 각각의 값에 new가 추가된 이유, 없으면 "undefined for the type" 오류 발생
 		Function lf = new Function(new Position(1,3), new Position(2,5));
 		
 		System.out.println(lf.printForm());
-		
 	}
-
 }
