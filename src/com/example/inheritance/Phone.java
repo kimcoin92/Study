617d5b88ff303c30163a0fba56c5ac3afa6c53c0
Kimcoin <kimcoin92@gmail.com>
2022-06-14 08:58:19 +0900

    FIX class 영역 수정

--- src/com/example/inheritance/Phone.java
+++ src/com/example/inheritance/Phone.java
@@ -14,18 +14,18 @@ class SmartPhone extends MobilePhone
 	{
 		System.out.println("Call");
 	}
-
-public class ExtendExample2 {
 	
-	public static void main(String[] args) {
-		
+}
+
+public class Phone {
+	
+	public static void main(String[] args)
+	{
 		MobilePhone phone1 = new MobilePhone();
 		MobilePhone phone2 = new SmartPhone();
 		
 		phone1.call();
 		phone2.call();
-		
 	}
 }
 
-}
